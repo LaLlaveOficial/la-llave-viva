c7dc30d60ae333fce6232005a5b9e648398ef876
LaLlaveOficial <contacto@lallaveoficial.com>
2026-08-23 22:22:32 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './ajraz10.css';
+import './ajraz10.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
